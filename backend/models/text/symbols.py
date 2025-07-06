f7864d356b15b65c82aa7ff51bd4d5a74a6a6d00
Kushagra Srivastava <165312755+KushagraSrivastava3028@users.noreply.github.com>
2025-07-06 15:47:30 +0530

    Create symbols.py

---
+++ backend/models/text/symbols.py
@@ -0,0 +1,11 @@
+symbols = [
+    '_', ',', '.', '!', '?', '-',
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+    'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+    'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+]
+
+# For compatibility
+_symbol_to_id = {s: i for i, s in enumerate(symbols)}
+_id_to_symbol = {i: s for i, s in enumerate(symbols)}
